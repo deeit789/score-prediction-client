4fd9a787b99a143e982bb6f10bfca71a20fab9e2
Dee <deeit789@gmail.com>
2022-12-03 15:18:15 +0700

    Dee

--- helpers/url_helper.js
+++ helpers/url_helper.js
@@ -2,7 +2,7 @@ export const API_MATCH_BY_DATE = "/matches/getmatchbydate";
 export const API_SYSTEM_TIME = "/system/time";
 export const API_MATCH = "/matchscore";
 export const API_ACCOUNT_ADMIN_DATA = "account/admin/data";
-export const API_MATCH_GET_DATE = "matches/getmatchbydate";
+export const API_MATCH_GET_DATE = "matchscore/getdate";
 export const API_MATCH_FIND_PLAYER = "matchscore/findplayer";
 
 //IP, FP
